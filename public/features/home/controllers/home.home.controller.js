68dff95b398da158e29c0f730e31e6fc47665566
Kiran Battaluri <kiran@unobridge.com>
2016-04-02 18:39:37 +0530

    Initiated Login page

--- public/features/home/controllers/home.home.controller.js
+++ public/features/home/controllers/home.home.controller.js
@@ -1,8 +1,25 @@
 angular.module("controllers")
-.controller("HomeController", ["$scope", 
-	function($scope){
+.controller("HomeController", ["$scope","$location","$window",
+	function($scope,$location,$window){
 		$scope.msg = "hello anks";
-
+		$scope.login = function () {
+		        authenticationSvc.login($scope.Email, $scope.password)
+		            .then(function (result) {
+		              $location.path("/#");
+		            }, function (error) {
+		                $window.alert("Invalid credentials");
+		                console.log(error);
+		            });
+		    };
+	   $scope.signup = function () {
+		        authenticationSvc.signup($scope.Email, $scope.password)
+		            .then(function (result) {
+		              $location.path("/#");
+		              }, function (error) {
+		                    $window.alert("error");
+		                    $location.path("/#");
+		              });
+           };
 		console.log("hello");
 	}
 ]);
