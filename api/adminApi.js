6c299f98cf410684bb7f6f4ba831db73e56e1a7a
ashok1994 <askipop@gmail.com>
2016-04-05 01:35:46 +0530

    no change

--- api/adminApi.js
+++ api/adminApi.js
@@ -18,6 +18,7 @@ exports.addUser = function( req , res){
         newUser.save(function(err ,  freshUser){
             if(err) { console.error('Database err'+ err); return res.status(500).send({message : 'Internal Server Error'});}
             return { console.info('User created '); return res.status(200).send({message : 'User created'});}
+
         });
 
     });
