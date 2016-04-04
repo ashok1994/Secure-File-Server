c1c5a8c76f15ca32aeb93ae9763c207796670dfa
Ankit <ankit_sony3@hotmail.com>
2016-04-05 01:11:37 +0530

    logout[WIP]

--- public/common/authSvc.js
+++ public/common/authSvc.js
@@ -22,6 +22,24 @@ angular.module('app.services')
 			return status;
 		};
 
+		this.logout = function(){
+			var access_token = localStorageService.get("access_token") || false;
+			if(access_token){
+				var deferred = $q.defer();
+				localStorageService.remove("access_token");
+				// TODO: Remove access_token in backend
+				$http({
+					method : "POST",
+					url    : "/api/logout",
+					headers: {access_token: access_token}
+				}).then(function(resp){
+					deferred.resolve(resp);
+				}, function(err){
+					deferred.reject(err);
+				});
+				return deferred.promise;
+			}
+		}
 
 		return (this);
 	}
